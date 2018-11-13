645b6a537f27ac356d86c318ab724288f952dc08
Steve Mackey <stevester94@gmail.com>
2018-11-12 23:37:50 -0800

    Working example of table, performance is acceptable

--- gui/electron/PerfMon/perf_mon/src/App.js
+++ gui/electron/PerfMon/perf_mon/src/App.js
@@ -1,39 +1,93 @@
 import React, { Component } from 'react';
-import './App.css';
+import ReactTable from "react-table";
 
-const { ipcRenderer } = window.require('electron');
+import './App.css';
+import 'react-table/react-table.css'
+
+// const { ipcRenderer } = window.require('electron');
 
 
 class NewApp extends Component {
   state = {
-    procs : null
+    procs : null,
+    intervalID : null
   };
+
+  gen_procs() {
+    let ret_array = []
+    for(let i = 0; i < 5000; i++)
+    {
+      ret_array.push(
+        {
+          pid: String(Math.floor(Math.random() * 10000)),
+          command: "Python",
+          UID: "1"
+        }
+      );
+    }
+
+    return ret_array;
+  }
+
+  updateState()
+  {
+    console.log("Updating");
+    this.setState(prevState =>({
+      procs: this.gen_procs()
+    }));
+  }
 
   constructor()
   {
     super();
     // In renderer process (web page).
 
-    ipcRenderer.on('procs-json-message', (event, arg) => {
-      console.log("Renderer received following from Main process:");
-      console.log(arg);
-      this.setState(prevState =>({
-        procs: JSON.stringify(arg)
-      }));
-    });
+    this.updateState = this.updateState.bind(this);
+    this.state.intervalID = setInterval(this.updateState, 5000);
+
+    this.state.procs = this.gen_procs();
+
+    // ipcRenderer.on('procs-json-message', (event, arg) => {
+    //   console.log("Renderer received following from Main process:");
+    //   console.log(arg);
+    //   this.setState(prevState =>({
+    //     procs: JSON.stringify(arg)
+    //   }));
+    // });
   }
 
   render() {
+    const { procs } = this.state;
     return (
-      <div className="App">
-        <p>
-          Chedd would love this!!<br/>
-          Last message received from main: {this.state.procs}
-        </p>
+      <div>
+        <ReactTable
+          data={procs}
+          columns={[
+            {
+              // Header: "Stuff",
+              columns: [
+                {
+                  Header: "PID",
+                  accessor: "pid"
+                },
+                {
+                  Header: "command",
+                  accessor: "command"
+                }
+              ]
+            }
+          ]}
+          defaultPageSize={5000}
+          className="-striped -highlight"
+        />
       </div>
     );
   }
 }
+
+
+
+
 
 export{
   NewApp
