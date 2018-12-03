8b7209e95671a096f156733964cf25a9b1e04d81
Steven Mackey <stevester94@gmail.com>
2018-12-02 22:50:58 -0800

    Data visualization looking sick AF

--- gui/electron/PerfMon/perf_mon/src/Historian2.js
+++ gui/electron/PerfMon/perf_mon/src/Historian2.js
@@ -12,17 +12,13 @@ am4core.useTheme(am4themes_animated);
 
 class Historian2 extends Component {
     componentDidMount() {
-        const {x_data, y_data} = this.props;
-        let chart = am4core.create("chartdiv", am4charts.XYChart);
+        this.chart = am4core.create("chartdiv", am4charts.XYChart);
         
         // let x_data = [10,300,12];
         // let y_data = [1,2,3];
 
-        chart.data = [];
-        for(var i = 0; i < x_data.length; i++)
-        {
-            chart.data.push({"category": x_data[i], "value": y_data[i]});
-        }
+        this.chart.data = [];
+
 
         // chart.data = [{
         //     "category": "1",
@@ -45,7 +41,7 @@ class Historian2 extends Component {
         // }];
 
         // Create axes
-        var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+        var categoryAxis = this.chart.xAxes.push(new am4charts.CategoryAxis());
         categoryAxis.dataFields.category = "category";
         categoryAxis.renderer.grid.template.location = 0;
         categoryAxis.renderer.minGridDistance = 30;
@@ -53,17 +49,17 @@ class Historian2 extends Component {
         categoryAxis.startLocation = 0.3;
         categoryAxis.endLocation = 0.7;
 
-        var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+        var valueAxis = this.chart.yAxes.push(new am4charts.ValueAxis());
 
         // Create series
-        var series = chart.series.push(new am4charts.LineSeries());
+        var series = this.chart.series.push(new am4charts.LineSeries());
         series.strokeWidth = 3;
         series.dataFields.valueY = "value";
         series.dataFields.categoryX = "category";
         series.tooltipText = "{value}";
 
 
-        chart.cursor = new am4charts.XYCursor();
+        this.chart.cursor = new am4charts.XYCursor();
     }
     
     componentWillUnmount() {
@@ -73,6 +69,22 @@ class Historian2 extends Component {
     }
     
     render() {
+        if(this.chart != null)
+        {
+            console.log("Setting data");
+            const { x_data, y_data } = this.props;
+            let data = [];
+            for (var i = 0; i < x_data.length; i++) {
+                data.push({ "category": x_data[i], "value": y_data[i] });
+            }
+
+            console.log(data);
+
+            this.chart.data = data;
+            this.chart.validateData();
+        }
+
+
         return (
             <div id="chartdiv" style={{ width: "100%", height: "500px" }}></div>
             );
